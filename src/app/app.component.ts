e9e5f1486a66ce90819cb22f2d73907d8436da36
AliceH <lotus.b78@gmail.com>
2021-10-22 01:38:11 +0200

    ajout componsant B

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { BComponent } from './b/b.component';
 import { Component } from '@angular/core';
-
+import { AComponent } from './a/a.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-sandbox';
+  title = 'app-SandBox';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AComponent
+    AComponent,
+    BComponent
   ],
   imports: [
     BrowserModule
